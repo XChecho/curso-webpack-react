26059e29a16a8adf668b7c7b63d6ab83b7f7e72b
Sergio Morales <sam94c@gmail.com>
2022-05-24 07:18:05 -0500

    Se instalan react y react dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./components/App.jsx";
-import "./styles/global.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import './styles/global.scss';
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
